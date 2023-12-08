6889f72496dcbec7282ce9d819c6e0878051da9f
Daniel Morales <danielmorales.arias23@gmail.com>
2023-12-08 18:02:19 -0300

    ADD Css

--- src/App.jsx
+++ src/App.jsx
@@ -17,11 +17,11 @@ function App(){
   } = useShop()
   return(
     <>
-    <div className="cart-list">
+    <div className="card-list">
       <h1>Lista de Compras</h1>
-      <div className="counter-shop">
-        <h3>N° de Compras:{shopsCount}</h3>
-        <h3>Pendientes:{pendingShopsCount}</h3>
+      <div className="counter-shops">
+        <h3>N° de Compras:<span>{shopsCount}</span></h3>
+        <h3>Pendientes:<span>{pendingShopsCount}</span></h3>
       </div>
       <div className="add-shop">
         <h3>Agregar Compra</h3>
